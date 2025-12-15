fea97be6d7ca0a1d0fee88cca288ed0acd72b55d
mayyu9 <coolmahimsd@gmail.com>
2025-12-15 12:11:29 +0530

    chat example with stream output

--- start-2.py
+++ start-2.py
@@ -10,7 +10,11 @@ res = ollama.chat(
     model="llama3.2:1b",
     messages=[
         {"role": "user", "content":"Explain how himalayas formed"}
-    ]
+    ],
+    stream=True
 )
 
-print(res["message"]["content"])
+#print(res["message"]["content"])
+
+for chunk in res:
+    print(chunk["message"]["content"], end='', flush=True)
