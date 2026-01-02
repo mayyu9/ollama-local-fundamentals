3f70796bbb1c6790d4f3f5d3a888b10aa50b2887
mayyu9 <coolmahimsd@gmail.com>
2026-01-02 13:41:45 +0530

    chunking and splitting the pdf

--- pdf-rag.py
+++ pdf-rag.py
@@ -26,6 +26,21 @@ else:
 
     # Preview first page
 content = data[0].page_content
-print(content[:200])
+# print(content[:200])
 
 # ==== End of PDF Ingestion ====
+
+
+# ==== Extract Text from PDF Files and Split into Small Chunks ====
+
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
+# Split and chunk
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1200, chunk_overlap=300)
+chunks = text_splitter.split_documents(data)
+print("done splitting....")
+
+print(f"Number of chunks: {len(chunks)}")
+print(f"Example chunk: {chunks[0]}")
